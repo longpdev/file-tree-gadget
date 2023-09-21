84a9d4831fd9ad559c3f1a65e36425d0eb82337a
Long <longpdev@gmail.com>
2023-09-20 20:35:37 -0600

    rewrite the code to practice

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const App = () => {
   return <div>
-    <Folder name="Desktop">
-      <Folder name="Music">
-        <File name="something.mp4"></File>
-        <File name="let_it_be.mp4"></File>
-
+    <Folder name="Documents">
+      <Folder name="Menu">
+        <File name="cat.jpeg"></File>
+        <File name="dog.jpeg"></File>
       </Folder>
       <File name="cat.jpeg"></File>
       <File name="dog.jpeg"></File>
     </Folder>
-    <Folder name="Application">
 
-    </Folder>
-
-  </div>
+    <Folder name="Application"></Folder>
+  </div >
 }
 
 const Folder = (props) => {
-  const [isOpen, setIsOpen] = useState(true);
-  const { name, children } = props;
-
-  const handleClick = () => {
-    setIsOpen(!isOpen)
-  };
+  const [name, children] = [props.name, props.children];
   return <div>
-    <span onClick={handleClick}>{name}</span>
+    {name}
     <div style={{ marginLeft: '17px' }}>
-      {isOpen ? children : null}
+      {children}
     </div>
-  </div >
 
+  </div >
 }
 
 const File = (props) => {
-  return <div>{props.name}</div>
+  const [name] = [props.name]
+  return <div>
+    {name}
+  </div>
 }
-
 
 export default App;

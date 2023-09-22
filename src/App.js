3365ca6dcb49ecb87a40157b008b0b0323a1fc17
Long <longpdev@gmail.com>
2023-09-22 12:44:02 -0600

    added style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 const App = () => {
   return <div>
     <Folder name="Documents">
-      <Folder name="Menu">
-        <File name="cat.jpeg"></File>
-        <File name="dog.jpeg"></File>
+      <Folder name="Music">
+        <File name="all_star.mp4"></File>
+        <File name="express_file.mp4"></File>
       </Folder>
-      <File name="cat.jpeg"></File>
-      <File name="dog.jpeg"></File>
+      <File name="cats.jpegs"></File>
+      <File name="dogs.jpegs"></File>
     </Folder>
 
     <Folder name="Application"></Folder>
@@ -16,24 +16,35 @@ const App = () => {
 }
 
 const Folder = (props) => {
-  const handleClick = () => {
-    setIsopen(!isOpen);
-  }
 
-  const [isOpen, setIsopen] = useState(false);
-  const [name, children] = [props.name, props.children];
+  const [isOpen, setIsopen] = useState(true);
+  const { name, children } = props;
+  const direction = isOpen ? 'down' : 'right';
+
+  const handleClick = () => setIsopen(!isOpen);
+
   return <div>
-    <span onClick={handleClick}>{name}</span>
+    <span onClick={handleClick}>
+      <i className="blue folder icon"></i>
+      <i className={`caret ${direction} icon`}></i>
+    </span>
+    {name}
     <div style={{ marginLeft: '17px' }}>
       {isOpen ? children : null}
     </div>
-
-  </div >
+  </div>
 }
 
 const File = (props) => {
   const [name] = [props.name]
+  const fileExtension = name.split('.')[1];
+  const fileIcons = {
+    mp4: 'headphones',
+    jpegs: 'file image',
+    png: 'file image outline'
+  };
   return <div>
+    <i className={`${fileIcons[fileExtension]} icon`}></i>
     {name}
   </div>
 }
